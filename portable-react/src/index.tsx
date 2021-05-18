5bb824cc73588b9ba4c59bf601cc7cf83e2e4195
ngduc <nducus@yahoo.com>
2021-05-18 06:23:28 +0100

    feat: Toast: add autoDismiss

--- portable-react/src/index.tsx
+++ portable-react/src/index.tsx
@@ -170,8 +170,9 @@ export type ToastProps = BaseProps & {
   title?: string;
   content?: any;
   onDismiss?: () => void;
+  autoDismiss?: number; // in (ms)
 };
-export const Toast = ({ className, success, error, title, content, icon, onDismiss, ...others }: ToastProps) => {
+export const Toast = ({ className, success, error, title, content, icon, onDismiss, autoDismiss, ...others }: ToastProps) => {
   const svgIcon = icon || (
     <svg className="h-6 w-6 text-teal mr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
       <path d="M2.93 17.07A10 10 0 1 1 17.07 2.93 10 10 0 0 1 2.93 17.07zm12.73-1.41A8 8 0 1 0 4.34 4.34a8 8 0 0 0 11.32 11.32zM9 11V9h2v6H9v-4zm0-6h2v2H9V5z" />
@@ -184,6 +185,14 @@ export const Toast = ({ className, success, error, title, content, icon, onDismi
   } else if (error) {
     cn = `${base} border-red-200`;
   }
+  const timerRef = React.useRef(0);
+  React.useEffect(() => {
+    if ((autoDismiss || 0) > 0) {
+      timerRef.current = setTimeout(() => (onDismiss ? onDismiss() : ''), autoDismiss);
+    }
+    return () => clearTimeout(timerRef.current);
+  }, []);
+
   return (
     <div className={`fixed bottom-4 right-4 w-5/6 md:w-full max-w-sm ${cn} ${className}`} role="alert" {...others}>
       <div className="flex" onClick={onDismiss}>
